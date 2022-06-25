5a4ee7104cc9556ab03cf840302e79f7d7b39114
LuongMinhNhatSE160865 <105339044+LuongMinhNhatSE160865@users.noreply.github.com>
2022-06-25 10:36:16 +0700

    ASS2_database

--- Asm02Solution/SalesWinApp/frmLogin.cs
+++ Asm02Solution/SalesWinApp/frmLogin.cs
@@ -6,5 +6,61 @@ namespace SalesWinApp
         {
             InitializeComponent();
         }
+
+        private void Emailtb_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Passwordtb_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void labelEmail_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            frmLogin frmLogin = new frmLogin();
+            this.Close();
+        }
+
+        private void labelEmail_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonLogin_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
     }
 }
