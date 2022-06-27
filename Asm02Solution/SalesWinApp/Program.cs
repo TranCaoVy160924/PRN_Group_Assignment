b28ec5f57ee67effcf0acfeae7a208afda215d0e
LuongMinhNhatSE160865 <105339044+LuongMinhNhatSE160865@users.noreply.github.com>
2022-06-27 09:31:21 +0700

    login

--- Asm02Solution/SalesWinApp/Program.cs
+++ Asm02Solution/SalesWinApp/Program.cs
@@ -11,8 +11,16 @@ namespace SalesWinApp
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+<<<<<<< HEAD
             Application.Run(new frmLogin());
+<<<<<<< Updated upstream
 
+=======
+=======
+            //Application.Run(new frmProducts());
+            Application.Run(new frmProducts());
+>>>>>>> dfa4c9c2f62db45674626db63599456af8ed6947
+>>>>>>> Stashed changes
         }
     }
 }
