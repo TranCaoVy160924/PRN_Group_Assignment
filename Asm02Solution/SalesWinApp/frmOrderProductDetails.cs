b8555b7e60115c230ccf49de0de17dadc2b580c6
LuongMinhNhatSE160865 <105339044+LuongMinhNhatSE160865@users.noreply.github.com>
2022-07-02 10:15:00 +0700

    orderDetail function
    testing (still bug)

--- Asm02Solution/SalesWinApp/frmOrderProductDetails.cs
+++ Asm02Solution/SalesWinApp/frmOrderProductDetails.cs
@@ -7,14 +7,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Ass2.BusinessObject;
+using Ass2.DataAccess.Repository;
+
 
 namespace SalesWinApp
 {
     public partial class frmOrderProductDetails : Form
     {
+        public OrderDetail orderDetailInfo = new OrderDetail();
+        public bool InsertOrUpdate { get; set; }//False: insert, true: update
+        public IOrderDetaiRepository OrderDetaiRepository { get; set; }
         public frmOrderProductDetails()
         {
             InitializeComponent();
         }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var orderDetail = new OrderDetail
+                {
+                    UnitPrice = decimal.Parse(txtUnitPrice.Text),
+                    Quantity = int.Parse(txtQuantity.Text),
+                    Discount = double.Parse(txtDiscount.Text),
+                };
+                if (InsertOrUpdate == false)
+                {
+                    //MessageBox.Show(member.MemberId.ToString(), "hello");
+                    OrderDetailRepository.InsertOrderDetail(orderDetail);
+                    OrderDetaiRepository.
+                }
+                else
+                {
+                    member.MemberId = int.Parse(txtID.Text);
+                    MemberRepository.UpdateMember(member);
+
+                }
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Insert");
+            }
+
+        }
+
+private void btnClose_Click(object sender, EventArgs e)
+        {
+
+        }
     }
-}
+            }
+    
+
